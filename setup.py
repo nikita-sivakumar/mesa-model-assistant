5b0d5861d347ea3ad208ae228089ea0dec31c8f3
nikita-sivakumar <ns4dg@virginia.edu>
2021-01-21 10:29:36 -0500

    setup.py updated

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mesa-model-assistant',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A package containing helper classes for batchruning agent-based models implemented in mesa.',
+    long_description=open('README.md').read(),
+    install_requires=['numpy',
+                        'pandas',
+                        'multiprocessing',
+                        'itertools',
+                        'mesa'
+                    ],
+    url="https://github.com/nikita-sivakumar/mesa-model-assistant.git",
+    author='Nikita Sivakumar',
+    author_email='nikita.sivakumar@gmail.com'
+)
